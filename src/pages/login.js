a6e598ae04baa9ce3f4350b92d0de70bdcdae2c6
Jordan Maslyn <jordan.maslyn@wpengine.com>
2022-05-25 11:19:42 -0400

    use next router for redirect

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,13 +1,15 @@
-import { client } from "client";
+import { client } from 'client';
+import { useRouter } from 'next/router';
 
 export default function Login() {
-    const { useAuth } = client.auth;
-    const { isAuthenticated, isLoading } = useAuth();
+  const { useAuth } = client.auth;
+  const { isAuthenticated, isLoading } = useAuth();
+  const router = useRouter();
 
-    if (!isAuthenticated) {
-        return <h1>Redirecting to login...</h1>
-    } else if (!isLoading) {
-        window?.location.href = '/';
-        return null;
-    }
+  if (!isAuthenticated) {
+    return <h1>Redirecting to login...</h1>;
+  } else if (!isLoading) {
+    router.push('/');
+    return null;
+  }
 }
